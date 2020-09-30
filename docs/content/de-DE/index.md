09b437fb69f3b1b83dd5bab86b922e654890047b
Pascal Luther <pascal@luther.ch>
2020-10-01 00:36:11 +0200

    feat(algolia): added Algolia search (test), and some fake locales to test i18n

---
+++ docs/content/de-DE/index.md
@@ -0,0 +1,12 @@
+---
+title: Deutschland ist gut
+description: 'Hallo Deutschland'
+---
+
+Ja hallo
+
+Bratwurst
+
+Bier
+
+Wunderbar
--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -10,5 +10,19 @@ export default theme({
     server: 'https://ackee.nuxtjs.com',
     domainId: '6abbd929-404a-41f4-b647-fae6f4f8131b',
     detailed: true
+  },
+  i18n: {
+    locales: () => [{
+      code: 'de-DE', // Info: Won't work wit algolia since docsearch is not setup for this language.
+      iso: 'de-DE',
+      file: 'de-DE.js',
+      name: 'Deutsch'
+    }, {
+      code: 'en-US', // Would be better to change docsearch to use "en" as locale instead of changing this
+      iso: 'en-US',
+      file: 'en-US.js',
+      name: 'English'
+    }],
+    defaultLocale: 'en-US'
   }
 })
